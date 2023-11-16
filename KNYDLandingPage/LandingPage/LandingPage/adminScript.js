c184695c4a1624dd4436bb38b50ba3db0ab3232e
Yeilin <yeilinmoya0208@gmail.com>
2023-11-15 21:43:16 -0600

    Fetch agredados

--- KNYDLandingPage/LandingPage/LandingPage/adminScript.js
+++ KNYDLandingPage/LandingPage/LandingPage/adminScript.js
@@ -1,26 +1,123 @@
-// Define un mapeo de secciones a elementos de esas secciones
-const sectionMap = {
-    header: ["companyName", "logo"],
-    titulo: ["Title", "description"],
-    logo: ["header.companyName", "header.logo"],
-    banner: ["titulo", "titulo_desc", "overlay"],
-    servicios: ["servicio_1", "desc_1", "img_1", "servicio_2", "desc_2", "img_2", "servicio_3", "desc_3", "img_3", "servicio_4", "desc_4", "img_4"],
-    galeria: ["imgG_1", "imgG_2", "imgG_3", "imgG_4", "imgG_5", "imgG_6", "imgG_7", "imgG_8"],
-    Preguntas: ["pregunta_1", "res_1", "pregunta_2", "res_2", "pregunta_3", "res_3", "pregunta_4", "res_4"],
-    contacto: ["nombre", "email", "asunto", "mensaje"],
-    footer: ["copy", "integrantes"],
-};
+// URL de la API para cada sección
+const companyApiUrl = 'http://localhost:3000/company';
+const servicesApiUrl = 'http://localhost:3000/servicio';
+const galeriaApiUrl = 'http://localhost:3000/galeria';
 
-const mainMenu = document.getElementById("mainMenu");
-const editContainer = document.getElementById("editContainer");
+// Función para actualizar la información principal de la empresa
+function updateCompanyInfo() {
+    const title = document.getElementById('company-title').value;
+    const description = document.getElementById('company-description').value;
+    const galeriaUrl = document.getElementById('company-galeria').value;
 
-mainMenu.addEventListener("click", (event) => {
-    const section = event.target.dataset.section;
-    displaySectionMenu(section);
-});
+    const data = {
+        title: title,
+        description: description,
+        galeriaUrl: galeriaUrl
+    };
 
-function displaySectionMenu(section) {
-    const sectionElements = sectionMap[section];
-    const sectionMenu = sectionElements.map(element => `<li><a href="#" data-element="${element}">Editar ${element}</a></li>`).join("");
-    editContainer.innerHTML = `<ul>${sectionMenu}</ul>`;
+    fetch(companyApiUrl, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Respuesta del servidor (Información Principal):', data);
+        // Puedes realizar acciones adicionales según la respuesta del servidor
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Función para actualizar o eliminar un servicio específico
+function updateService(serviceId) {
+    const serviceName = document.getElementById(`service${serviceId}-name`).value;
+    const serviceDescription = document.getElementById(`service${serviceId}-description`).value;
+
+    const data = {
+        name: serviceName,
+        description: serviceDescription
+    };
+
+    fetch(`${servicesApiUrl}/${serviceId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(`Respuesta del servidor (Servicio ${serviceId}):`, data);
+        // Puedes realizar acciones adicionales según la respuesta del servidor
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function deleteService(serviceId) {
+    fetch(`${servicesApiUrl}/${serviceId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(`Respuesta del servidor (Eliminar Servicio ${serviceId}):`, data);
+        // Puedes realizar acciones adicionales según la respuesta del servidor
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Función para actualizar o eliminar imágenes
+function modificarGaleria() {
+    const selectedImages = document.querySelectorAll('input[name^="img-delete"]:checked');
+
+    // Construir un array con los IDs de las imágenes seleccionadas
+    const imageIds = Array.from(selectedImages).map(image => image.id.replace('img', ''));
+
+    const data = {
+        imageIds: imageIds
+    };
+
+    fetch(galeriaApiUrl, {
+        method: 'PUT', // Puedes cambiar el método según las necesidades de tu API
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Respuesta del servidor (Modificar Imágenes):', data);
+        // Puedes realizar acciones adicionales según la respuesta del servidor
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function deleteGaleria() {
+    // Implementa la lógica para eliminar imágenes
+    fetch(galeriaApiUrl, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Respuesta del servidor (Eliminar Imágenes):', data);
+        // Puedes realizar acciones adicionales según la respuesta del servidor
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
