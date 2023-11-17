6ff07e5f8d1918b3d42dadc34b453b7947c5d65e
Yeilin <yeilinmoya0208@gmail.com>
2023-11-17 00:55:55 -0600

    admi-actualizado

--- KNYDLandingPage/LandingPage/LandingPage/adminScript.js
+++ KNYDLandingPage/LandingPage/LandingPage/adminScript.js
@@ -1,123 +1,129 @@
 // URL de la API para cada sección
-const companyApiUrl = 'http://localhost:3000/company';
-const servicesApiUrl = 'http://localhost:3000/servicio';
+const companyApiUrl = 'http://localhost:3000/api/companies';
+const servicesApiUrl = 'http://localhost:3000/servicios';
 const galeriaApiUrl = 'http://localhost:3000/galeria';
 
 // Función para actualizar la información principal de la empresa
 function updateCompanyInfo() {
     const title = document.getElementById('company-title').value;
     const description = document.getElementById('company-description').value;
-    const galeriaUrl = document.getElementById('company-galeria').value;
+    const galeriaUrl = document.getElementById('company-image').value;
 
-    const data = {
-        title: title,
-        description: description,
-        galeriaUrl: galeriaUrl
-    };
 
-    fetch(companyApiUrl, {
-        method: 'PUT',
+    fetch(`${companyApiUrl}/1`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+
+            id: 1,
+    
+            title: title,
+    
+            Descripcion: description,
+    
+            Headerlogo: galeriaUrl,
+
+
+        }),
         headers: {
-            'Content-Type': 'application/json'
+            'Content-type': 'application/json; charset=UTF-8',
         },
-        body: JSON.stringify(data)
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Respuesta del servidor (Información Principal):', data);
-        // Puedes realizar acciones adicionales según la respuesta del servidor
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            console.log('Respuesta del servidor (Información Principal):', data);
+            // Puedes realizar acciones adicionales según la respuesta del servidor
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
-// Función para actualizar o eliminar un servicio específico
-function updateService(serviceId) {
-    const serviceName = document.getElementById(`service${serviceId}-name`).value;
-    const serviceDescription = document.getElementById(`service${serviceId}-description`).value;
+// // Función para actualizar o eliminar un servicio específico
+// function updateService(serviceId) {
+//     const serviceName = document.getElementById(`service${serviceId}-name`).value;
+//     const serviceDescription = document.getElementById(`service${serviceId}-description`).value;
 
-    const data = {
-        name: serviceName,
-        description: serviceDescription
-    };
+//     const data = {
+//         name: serviceName,
+//         description: serviceDescription
+//     };
 
-    fetch(`${servicesApiUrl}/${serviceId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(`Respuesta del servidor (Servicio ${serviceId}):`, data);
-        // Puedes realizar acciones adicionales según la respuesta del servidor
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+//     fetch(`${servicesApiUrl}/${serviceId}`, {
+//         method: 'PUT',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(`Respuesta del servidor (Servicio ${serviceId}):`, data);
+//             // Puedes realizar acciones adicionales según la respuesta del servidor
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+// }
 
-function deleteService(serviceId) {
-    fetch(`${servicesApiUrl}/${serviceId}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(`Respuesta del servidor (Eliminar Servicio ${serviceId}):`, data);
-        // Puedes realizar acciones adicionales según la respuesta del servidor
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+// function deleteService(serviceId) {
+//     fetch(`${servicesApiUrl}/${serviceId}`, {
+//         method: 'DELETE',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(`Respuesta del servidor (Eliminar Servicio ${serviceId}):`, data);
+//             // Puedes realizar acciones adicionales según la respuesta del servidor
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+// }
 
-// Función para actualizar o eliminar imágenes
-function modificarGaleria() {
-    const selectedImages = document.querySelectorAll('input[name^="img-delete"]:checked');
+// // Función para actualizar o eliminar imágenes
+// function modificarGaleria() {
+//     const selectedImages = document.querySelectorAll('input[name^="img-delete"]:checked');
 
-    // Construir un array con los IDs de las imágenes seleccionadas
-    const imageIds = Array.from(selectedImages).map(image => image.id.replace('img', ''));
+//     // Construir un array con los IDs de las imágenes seleccionadas
+//     const imageIds = Array.from(selectedImages).map(image => image.id.replace('img', ''));
 
-    const data = {
-        imageIds: imageIds
-    };
+//     const data = {
+//         imageIds: imageIds
+//     };
 
-    fetch(galeriaApiUrl, {
-        method: 'PUT', // Puedes cambiar el método según las necesidades de tu API
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Respuesta del servidor (Modificar Imágenes):', data);
-        // Puedes realizar acciones adicionales según la respuesta del servidor
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+//     fetch(galeriaApiUrl, {
+//         method: 'PUT', // Puedes cambiar el método según las necesidades de tu API
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log('Respuesta del servidor (Modificar Imágenes):', data);
+//             // Puedes realizar acciones adicionales según la respuesta del servidor
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+// }
 
-function deleteGaleria() {
-    // Implementa la lógica para eliminar imágenes
-    fetch(galeriaApiUrl, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Respuesta del servidor (Eliminar Imágenes):', data);
-        // Puedes realizar acciones adicionales según la respuesta del servidor
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+// function deleteGaleria() {
+//     // Implementa la lógica para eliminar imágenes
+//     fetch(galeriaApiUrl, {
+//         method: 'DELETE',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log('Respuesta del servidor (Eliminar Imágenes):', data);
+//             // Puedes realizar acciones adicionales según la respuesta del servidor
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+//}
