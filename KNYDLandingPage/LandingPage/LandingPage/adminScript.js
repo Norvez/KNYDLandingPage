2774773a38063858b65bee042484873c6ae19e93
Yeilin <yeilinmoya0208@gmail.com>
2023-11-16 21:35:41 -0600

    ajajaj

--- KNYDLandingPage/LandingPage/LandingPage/adminScript.js
+++ KNYDLandingPage/LandingPage/LandingPage/adminScript.js
@@ -1,7 +1,50 @@
+// URL base común
+const apiBaseUrl = 'http://localhost:3000';
+
 // URL de la API para cada sección
-const companyApiUrl = 'http://localhost:3000/company';
-const servicesApiUrl = 'http://localhost:3000/servicio';
-const galeriaApiUrl = 'http://localhost:3000/galeria';
+const companyApiUrl = `$http://localhost:3000/api/companies`;
+const servicesApiUrl = `$http://localhost:3000/servicios`;
+const galeriaApiUrl = `$http://localhost:3000/galeria`;
+
+// Detectar cuando se carga la página completamente
+window.addEventListener("load", (event) => {
+    let logoImg = document.getElementById("logo");
+    let nameHeader = document.getElementById("siglas");
+    let h2_cabecera = document.getElementById("titulo");
+    let h2_desc = document.getElementById("titulo_desc");
+    let footerCopy = document.getElementById("copy");
+    let footerInteg = document.getElementById("integrantes");
+
+    console.log("page is fully loaded");
+
+    fetch(`$http://localhost:3000/api/companies`)  // Utilizar la nueva URL
+    .then(response => response.json())
+    .then(company => {
+        logoImg.src = company.header.logo;
+        nameHeader.textContent = company.header.companyName;
+        h2_cabecera.textContent = company.Title;
+        h2_desc.textContent = company.description;
+        footerCopy.textContent = company.footer.copyrightText;
+        footerInteg.textContent = company.footer.teamMembers;
+
+        console.log(company, company.services);
+
+        company.services.forEach(service => {
+            document.getElementById(`desc_${service.id}`).innerText = service.description;
+            document.getElementById(`img_${service.id}`).src = service.ico;
+            document.getElementById(`servicio_${service.id}`).innerText = service.type;
+        });
+
+        company.gallery.forEach(gallery => {
+            document.getElementById(`imgG_${gallery.id}`).src = gallery.image;
+        });
+
+        company.faq.forEach(faq => {
+            document.getElementById(`pregunta_${faq.id}`).innerText = faq.question;
+            document.getElementById(`res_${faq.id}`).innerText = faq.answer;
+        });
+    });
+});
 
 // Función para actualizar la información principal de la empresa
 function updateCompanyInfo() {
@@ -15,7 +58,7 @@ function updateCompanyInfo() {
         galeriaUrl: galeriaUrl
     };
 
-    fetch(companyApiUrl, {
+    fetch('http://localhost:3000/api/companies', {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
@@ -31,8 +74,7 @@ function updateCompanyInfo() {
         console.error('Error:', error);
     });
 }
-
-// Función para actualizar o eliminar un servicio específico
+// Resto de las funciones (updateService, deleteService, modificarGaleria, deleteGaleria)...
 function updateService(serviceId) {
     const serviceName = document.getElementById(`service${serviceId}-name`).value;
     const serviceDescription = document.getElementById(`service${serviceId}-description`).value;
@@ -42,7 +84,7 @@ function updateService(serviceId) {
         description: serviceDescription
     };
 
-    fetch(`${servicesApiUrl}/${serviceId}`, {
+    fetch(`$http://localhost:3000/servicios}/${serviceId}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
@@ -60,7 +102,7 @@ function updateService(serviceId) {
 }
 
 function deleteService(serviceId) {
-    fetch(`${servicesApiUrl}/${serviceId}`, {
+    fetch(`$http://localhost:3000/servicios}/${serviceId}`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
@@ -76,37 +118,9 @@ function deleteService(serviceId) {
     });
 }
 
-// Función para actualizar o eliminar imágenes
-function modificarGaleria() {
-    const selectedImages = document.querySelectorAll('input[name^="img-delete"]:checked');
-
-    // Construir un array con los IDs de las imágenes seleccionadas
-    const imageIds = Array.from(selectedImages).map(image => image.id.replace('img', ''));
-
-    const data = {
-        imageIds: imageIds
-    };
-
-    fetch(galeriaApiUrl, {
-        method: 'PUT', // Puedes cambiar el método según las necesidades de tu API
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Respuesta del servidor (Modificar Imágenes):', data);
-        // Puedes realizar acciones adicionales según la respuesta del servidor
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
-
 function deleteGaleria() {
     // Implementa la lógica para eliminar imágenes
-    fetch(galeriaApiUrl, {
+    fetch('http://localhost:3000/galeria', {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
--- KNYDLandingPage/LandingPage/LandingPage/main.js
+++ KNYDLandingPage/LandingPage/LandingPage/main.js
@@ -1,47 +1,75 @@
 // Detectar cuando se carga la página completamente
 window.addEventListener("load", (event) => {
 
-    let logoImg = document.getElementById("logo");
-    let nameHeader = document.getElementById("siglas");
+  let logoImg = document.getElementById("logo");
+  let nameHeader = document.getElementById("siglas");
 
-    let h2_cabecera = document.getElementById("titulo");
-    let h2_desc = document.getElementById("titulo_desc");
+  let h2_cabecera = document.getElementById("titulo");
+  let h2_desc = document.getElementById("titulo_desc");
 
-    let footerCopy = document.getElementById("copy");
-    let footerInteg = document.getElementById("integrantes");
+  let footerCopy = document.getElementById("copy");
+  let footerInteg = document.getElementById("integrantes");
 
-    console.log("page is fully loaded");
+  console.log("page is fully loaded");
 
-    fetch('./js/mocks/company.json') 
-    .then(response => response.json())
-    .then(company => {
+  fetch('http://localhost:3000/api/companies') 
+  .then(response => response.json())
+  .then(data => {
 
+    const company = data[0]; // Acceder al primer elemento del array, si la respuesta es un array
 
-      logoImg.src = company.header.logo;
-      nameHeader.textContent = company.header.companyName;
-      h2_cabecera.textContent = company.Title;
-      h2_desc.textContent = company.description;
-      footerCopy.textContent = company.footer.copyrightText;
-      footerInteg.textContent = company.footer.teamMembers;
+      console.log(company);
 
+      logoImg.src = company.Headerlogo; // Ajustar a la propiedad correcta
+      nameHeader.textContent = company.HeaderCompanyName;
+      h2_cabecera.textContent = company.title;
+      h2_desc.textContent = company.Descripcion;
+      footerCopy.textContent = company.FooterCopyRightTex;
+      footerInteg.textContent = company.FooterteamMenbers;
 
-      console.log(company, company.services);
+      console.log(company, company.servicios);
 
-      company.services.forEach(service => {
-        document.getElementById(`desc_${service.id}`).innerText = service.description;
-        document.getElementById(`img_${service.id}`).src = service.ico;
-        document.getElementById(`servicio_${service.id}`).innerText = service.type;
+      company.servicios.forEach(service => {
+        document.getElementById('desc_${service.id}').innerText = service.Descripcion; // Ajustar a la propiedad correcta
+        document.getElementById('img_${service.id}').src = service.IcoUrl; // Ajustar a la propiedad correcta
+        document.getElementById('servicio_${service.id}').innerText = service.Type; // Ajustar a la propiedad correcta
+          /*document.getElementById(desc_${service.id}).innerText = service.descripcion; // Ajustar a la propiedad correcta
+          document.getElementById(img_${service.id}).src = service.imagen; // Ajustar a la propiedad correcta
+          document.getElementById(servicio_${service.id}).innerText = service.tipo; // Ajustar a la propiedad correcta
+          */
       });
 
-      company.gallery.forEach(gallery => {
-        document.getElementById(`imgG_${gallery.id}`).src = gallery.image;
+      company.galerias.forEach(gallery => {
+          document.getElementById('imgG_${gallery.id}').src = gallery.imageUrl; // Ajustar a la propiedad correcta
       });
 
-      company.faq.forEach(faq => {
-        document.getElementById(`pregunta_${faq.id}`).innerText = faq.question;
-        document.getElementById(`res_${faq.id}`).innerText = faq.answer;
-      });
+    /*console.log(company);
 
-    })
+    logoImg.src = company.header.logo;
+    nameHeader.textContent = company.header.companyName;
+    h2_cabecera.textContent = company.Title;
+    h2_desc.textContent = company.description;
+    footerCopy.textContent = company.footer.copyrightText;
+    footerInteg.textContent = company.footer.teamMembers;
+
+
+    console.log(company, company.services);
+
+    company.services.forEach(service => {
+      document.getElementById(desc_${service.id}).innerText = service.description;
+      document.getElementById(img_${service.id}).src = service.ico;
+      document.getElementById(servicio_${service.id}).innerText = service.type;
+    });
+
+    company.gallery.forEach(gallery => {
+      document.getElementById(imgG_${gallery.id}).src = gallery.imageUrl;
+    });*/
+
+    company.faq.forEach(faq => {
+      document.getElementById('pregunta_${faq.id}').innerText = faq.question;
+      document.getElementById('res_${faq.id}').innerText = faq.answer;
+    });
+
+  })
 
 });
