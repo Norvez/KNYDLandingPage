759fc569e81f0271bf640676664f7dd997cb5cd8
KarlaAnguloC <angulokarla1201@gmail.com>
2023-11-17 09:51:17 -0600

    Ajuste-actulizar-eliminar

--- KNYDLandingPage/LandingPage/LandingPage/adminScript.js
+++ KNYDLandingPage/LandingPage/LandingPage/adminScript.js
@@ -3,7 +3,6 @@ const companyApiUrl = 'http://localhost:3000/api/companies';
 const servicesApiUrl = 'http://localhost:3000/servicios';
 const galeriaApiUrl = 'http://localhost:3000/galeria';
 
-// Función para actualizar la información principal de la empresa
 function updateCompanyInfo() {
     const title = document.getElementById('company-title').value;
     const description = document.getElementById('company-description').value;
@@ -31,99 +30,49 @@ function updateCompanyInfo() {
         .then(response => response.json())
         .then(data => {
             console.log('Respuesta del servidor (Información Principal):', data);
-            // Puedes realizar acciones adicionales según la respuesta del servidor
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
 
-// // Función para actualizar o eliminar un servicio específico
-// function updateService(serviceId) {
-//     const serviceName = document.getElementById(`service${serviceId}-name`).value;
-//     const serviceDescription = document.getElementById(`service${serviceId}-description`).value;
+function updateService() {
+    const serviceName = document.getElementById('service-name').value;
+    const serviceDescription = document.getElementById('service-description').value;
+    const serviceIdToUpdate = document.getElementById('service-id').value;
 
-//     const data = {
-//         name: serviceName,
-//         description: serviceDescription
-//     };
+    fetch(`${servicesApiUrl}/${serviceIdToUpdate}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            id: serviceIdToUpdate,
+            Type: serviceName,
+            Descripcion: serviceDescription,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(`Respuesta del servidor (Actualización de Servicio):`, data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
 
-//     fetch(`${servicesApiUrl}/${serviceId}`, {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(`Respuesta del servidor (Servicio ${serviceId}):`, data);
-//             // Puedes realizar acciones adicionales según la respuesta del servidor
-//         })
-//         .catch(error => {
-//             console.error('Error:', error);
-//         });
-// }
+function deleteService() {
+    const serviceIdToDelete = document.getElementById('service-id').value;
 
-// function deleteService(serviceId) {
-//     fetch(`${servicesApiUrl}/${serviceId}`, {
-//         method: 'DELETE',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(`Respuesta del servidor (Eliminar Servicio ${serviceId}):`, data);
-//             // Puedes realizar acciones adicionales según la respuesta del servidor
-//         })
-//         .catch(error => {
-//             console.error('Error:', error);
-//         });
-// }
+    fetch(`${servicesApiUrl}/${serviceIdToDelete}`, {
+        method: 'DELETE',
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(`Respuesta del servidor (Eliminación de Servicio):`, data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
 
-// // Función para actualizar o eliminar imágenes
-// function modificarGaleria() {
-//     const selectedImages = document.querySelectorAll('input[name^="img-delete"]:checked');
-
-//     // Construir un array con los IDs de las imágenes seleccionadas
-//     const imageIds = Array.from(selectedImages).map(image => image.id.replace('img', ''));
-
-//     const data = {
-//         imageIds: imageIds
-//     };
-
-//     fetch(galeriaApiUrl, {
-//         method: 'PUT', // Puedes cambiar el método según las necesidades de tu API
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log('Respuesta del servidor (Modificar Imágenes):', data);
-//             // Puedes realizar acciones adicionales según la respuesta del servidor
-//         })
-//         .catch(error => {
-//             console.error('Error:', error);
-//         });
-// }
-
-// function deleteGaleria() {
-//     // Implementa la lógica para eliminar imágenes
-//     fetch(galeriaApiUrl, {
-//         method: 'DELETE',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log('Respuesta del servidor (Eliminar Imágenes):', data);
-//             // Puedes realizar acciones adicionales según la respuesta del servidor
-//         })
-//         .catch(error => {
-//             console.error('Error:', error);
-//         });
-//}
